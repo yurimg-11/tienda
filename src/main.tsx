9a9d7923af98666ab9097a8273bf9ac295cf3ecd
yurimg-11 <yologm@hotmail.com>
2026-08-06 15:43:06 -0600

    Fix: import index.css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+// @ts-ignore: allow side-effect CSS import without type declarations
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
